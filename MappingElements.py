20fb1289a8ce415bbd6ffe1f6c4d3c7e2a7ccf9f
HardintTech <alexandre.biscorray@gmail.com>
2023-10-02 11:38:55 +0200

    mapping elements from text pushed

---
+++ MappingElements.py
@@ -0,0 +1,15 @@
+# Ask user to put his decklist
+
+texte = input("Entrez un texte : ")
+
+
+#Create dictionnary with card name for key and card number for value
+map={}
+
+#Split elements from optcgsim import decklist and put it in a map
+listeText = texte.split(" ")
+for element in listeText:
+  info = element.split("x")
+  map[info[1]] = info[0]
+
+# print(dico)
